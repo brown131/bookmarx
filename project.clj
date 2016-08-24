2b1cf0986e781b35fa4a4ea361529dc85ca0a10c
Scott Brown <brown131@yahoo.com>
2016-08-23 22:16:58 -0500

    Revert "Added bootstap-submenu to project."
    This reverts commit f02cc92816c1b9a8707df2c0762d828636f1acaf.

--- project.clj
+++ project.clj
@@ -50,9 +50,7 @@
 
   :minify-assets
   {:assets
-   {"resources/public/css/site.min.css" "resources/public/css/site.css"
-    "resources/public/css/bootstrap-submenu/bootstrap-submenu.min.css"
-    "resources/public/css/bootstrap-submenu/bootstrap-submenu.css"}}
+   {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
 
   :cljsbuild
   {:builds {:min
--- src/cljs/bookmarx/header.cljs
+++ src/cljs/bookmarx/header.cljs
@@ -1,17 +1,8 @@
 (ns bookmarx.header
   (:require [reagent.session :as session]
-            [goog.dom :as dom]
             [bookmarx.common :refer [env]]))
 
 (enable-console-print!)
-
-(defn style-elem
-  "Style an element"
-  [element attrs]
-  (dom/setProperties element
-                     (js-obj "style" (apply str (interpose ";"
-                                                             (map #(str (name %1) ":" %2)
-                                                                  (keys attrs) (vals attrs)))))))
 
 (defn header "Render the header for the page."
   ([] (header false))
@@ -34,17 +25,5 @@
            [:li [:a {:href (str (:prefix env) "/about")} "About..."]]
            [:li [:a {:href (str (:prefix env) "/add") 
                      :on-click #(session/remove! :add)} "Add Bookmark..."]]
-           [:li.dropdown-submenu
-             {:on-click (fn [e]
-                             ;;(.log js/console (dom/getElement "toggle-item"))
-                             (style-elem (dom/getElement "toggle-item") {:display "none"})
-                             ;;(.toggle (dom/getElement "show-item"))
-                             (.stopPropagation e)
-                             (.preventDefault e)
-                             false)} "Show" [:span.caret]
-                [:ul
-                  [:li {:id "toggle-item"} [:a {:href "#"} "URL"]]
-                  [:li [:a {:href "#"} "Created"]]
-                  [:li [:a {:href "#"} "Last Visited"]]
-                  [:li [:a {:href "#"} "Visited"]]]]
+           [:li [:a {:href "#"} "Show"]]
            [:li [:a {:href "#"} "Sort"]]]]]])]]]))

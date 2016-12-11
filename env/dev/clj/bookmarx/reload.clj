34fe4adc84c6143d91b216af49a22e71b642cf92
Scott Brown <brown131@yahoo.com>
2016-12-11 17:09:30 -0600

    Added script to reload downloaded bookmarks

---
+++ env/dev/clj/bookmarx/reload.clj
@@ -0,0 +1,68 @@
+;;;; Reload bookmarks from a bookmarks download file.
+(ns bookmarx.reload
+  (:gen-class))
+
+;; Load dependencies.
+(use '[leiningen.exec :only (deps)])
+(deps '[[org.clojure/java.jdbc "0.6.1"]
+        [mysql/mysql-connector-java "5.1.21"]])
+(deps '[[com.datomic/datomic-pro "0.9.5350" :exclusions [joda-time]]]
+      :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo" :creds :gpg}})
+
+;; Require packages.
+(require '[clojure.java.jdbc :as j])
+(require '[datomic.api :refer [connect create-database db delete-database q squuid transact]])
+
+(#'clojure.core/load-data-readers)
+(set! *data-readers* (.getRawRoot #'*data-readers*))
+
+;;; Recreate Bookmarx database.
+(def uri "datomic:sql://bookmarx?jdbc:mysql://localhost:3306/datomic?user=datomic&password=datomic")
+(delete-database uri)
+(create-database uri)
+(def schema-tx (read-string (slurp "resources/data/schema.edn")))
+(def conn (connect uri))
+@(transact conn schema-tx)
+
+(def download (flatten (read-string (slurp "resources/data/bookmarks.edn"))))
+
+(def root (first (filter #(nil? (:bookmark/parent %)) download)))
+
+(def old-dbids (reduce #(assoc %1 (:db/id %2) %2) {} download))
+
+(def dbids (atom {}))
+
+(def retry (atom []))
+
+(def bookmarkids (apply merge (map #(merge {(:bookmark/id %) %}
+                                           (apply merge (map (fn [b] {(:bookmark/id b) b})
+                                                             (:bookmark/_parent %)))) download)))
+(defn get-bookmarks [bookmark]
+  (vec (reduce #(if (:bookmark/url %2)
+                  (conj %1 %2)
+                  (concat %1 (get-bookmarks (get old-dbids (:db/id %2)))))
+               [bookmark] (:bookmark/_parent bookmark))))
+
+(defn prepare-bookmark [bookmark]
+  (-> bookmark
+      (dissoc :bookmark/_parent)
+      (update-in [:db/id] (fn [_] #db/id[:db.part/user]))
+      (#(if (:bookmark/parent %)
+          (update-in % [:bookmark/parent :db/id] (fn [b] (get @dbids b)))
+          %))))
+
+(defn load-bookmarks [bookmarks]
+  (dorun (map #(if (or (not (:bookmark/parent %))
+                       (get @dbids (get-in % [:bookmark/parent :db/id])))
+                 (let [response @(transact conn [(prepare-bookmark %)])
+                       dbid (second (first (vec (:tempids response))))]
+                   (println "Reloading " (:db/id %))
+                   (swap! dbids assoc (:db/id %) dbid))
+                 (do (swap! retry conj %) nil)) bookmarks)))
+
+(load-bookmarks (get-bookmarks root))
+
+(while (not (empty? @retry))
+  (let [bookmarks @retry]
+    (reset! retry [])
+    (load-bookmarks bookmarks)))
---
+++ env/dev/script/run-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+# Run from bookmarx/ as: env/dev/script/run-script.sh scriptname
+
+GPG_AGENT_PATH=/tmp/$(ls /tmp | grep gpg- )/S.gpg-agent
+GPG_AGENT_PID=$(ps -A | grep gpg- | sed -e 's/^ *//' | cut -f1 -d" " | head -1)
+
+export GPG_AGENT_INFO=$GPG_AGENT_PATH:$GPG_AGENT_PID:1
+export GPG_TTY=$(tty)
+
+# Reset the agent symlink
+rm ~/.gnupg/S.gpg-agent
+ln -s $GPG_AGENT_PATH ~/.gnupg/S.gpg-agent
+
+#KEYGRIP=$(gpg --fingerprint --fingerprint| grep fingerprint | tail -1 | cut -d= -f2 | sed -e 's/ //g')
+#/usr/local/bin/gpg-preset-passphrase --passphrase $(base64 --decode ~/.gnupg/preset) --preset $KEYGRIP
+
+#gpg --quiet --batch --decrypt ~/.lein/credentials.clj.gpg
+
+lein exec env/dev/clj/bookmarx/$1.clj

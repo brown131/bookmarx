5a3d627dc5bd0b5664c948a6a0d85b9b0644a3d6
Scott Brown <brown131@yahoo.com>
2017-01-08 13:29:36 -0600

    Backed out macros. They’re goofy in CLJS.

--- src/cljs/bookmarx/folder.cljs
+++ src/cljs/bookmarx/folder.cljs
@@ -15,7 +15,8 @@
                      :on-click #(session/update-in! [:add :bookmark/parent-id] (fn [_] id))
                      :href (str (:prefix env) "/add")} title])
      [:ul.nav.nav-pills.nav-stacked {:key (str id "-children-key")}
-      (doall (map #(bookmark-tree (session/get %)) (remove #(or (map? %) (= % add-id)) children)))]]))
+      (doall (map #(bookmark-tree (session/get %))
+                  (remove #(or (:bookmark/url (session/get %)) (= % add-id)) children)))]]))
 
 (defn folder-page "Select a parent folder for a bookmark."
   []

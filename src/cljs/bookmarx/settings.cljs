e5660648853bf8a4a791dbd386165d73a8d36e22
Scott Brown <brown131@yahoo.com>
2017-01-29 15:31:59 -0600

    Update last visited and visits when links are clicked.

---
+++ src/cljs/bookmarx/settings.cljs
@@ -0,0 +1,35 @@
+(ns bookmarx.settings
+  (:require [reagent.core :refer [atom]]
+            [reagent.session :as session]
+            [reagent-forms.core :refer [bind-fields]]
+            [bookmarx.client :refer [login]]
+            [bookmarx.header :as header]))
+
+  (defn row
+        [label input]
+        [:div.row
+         [:div.col-sm-2 ^{:key label} [:label label]]
+         [:div.col-sm-5 ^{:key input} input]])
+
+  (defn form-template
+        [doc]
+        [:div
+         ;; Show: Title URL Created "Last Visited" Visits Rating New Visited
+         ;; Sort-On: Title URL Created "Last Visited" Visits Rating "Link Count"
+         ;; Advanced: Password "New Hours" "Visited Hours"
+         [:div {:field :container}
+          [row "User" [:input.form-control {:field :text :id :user}]]
+          [row "Password" [:input.form-control {:field :password :id :password}]]]])
+
+  (defn editor [doc & body]
+        [:div body
+         (when (:error @doc)
+           [:div.alert.alert-danger (:error @doc)])
+         [:button.btn.btn-default {:on-click #(login doc)} "Login"]])
+
+  (defn login-page "Render the Login page."
+        []
+        [:div.col-sm-12
+         [header/header]
+         (let [doc (atom {:user "" :password ""})]
+           [editor doc [bind-fields (form-template doc) doc]])])

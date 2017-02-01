96af96eff1ab8f57843e55be58181d1251c4653a
Scott Brown <brown131@yahoo.com>
2017-01-31 19:53:49 -0600

    Added bootstrap and jquery to the project.

--- src/clj/bookmarx/pages.clj
+++ src/clj/bookmarx/pages.clj
@@ -11,7 +11,7 @@
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
    [:link {:rel "icon" :type "image/png" :href "favicon.ico"}]
    (include-css (if (env :dev) "css/site.css" "css/site.min.css"))
-   (include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")])
+   (include-css "bootstrap/3.3.7/css/bootstrap.min.css")])
 
 (def page-template
   (html5
@@ -19,8 +19,8 @@
     [:body {:class "body-container"}
      [:div#app]
      (include-js "js/app.js")
-     (include-js "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")
-     (include-js "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js")]))
+     (include-js "jquery/3.1.1/jquery.min.js")
+     (include-js "bootstrap/3.3.7/js/bootstrap.min.js")]))
 
 (defn login-page-handler
   [request]

fcb44d4f2fb13e692ce982b977bb086ed5371269
Scott Brown <brown131@yahoo.com>
2017-03-25 03:49:30 -0500

    Backed out broken version of ClojureScript.

--- env/dev/script/debug.sh
+++ env/dev/script/debug.sh
@@ -4,7 +4,10 @@ if [ -f bookmarx.pid ]; then
     kill -9 $(cat bookmarx.pid)
 fi
 
+cp ~/.m2/repository/org/bouncycastle/bcprov-jdk15on/1.55/bcprov-jdk15on-1.55.jar ~/.m2/repository/org/bouncycastle/bcpkix-jdk15on/1.55/bcpkix-jdk15on-1.55.jar target
+
 DEBUG=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
 CLASSPATH=target/bookmarx.jar:target/bcpkix-jdk15on-1.55.jar:target/bcprov-jdk15on-1.55.jar
 
-nohup java $DEBUG -cp $CLASSPATH clojure.main -m bookmarx.server & echo $! > bookmarx.pid
+nohup java $DEBUG -cp $CLASSPATH clojure.main -m bookmarx.server > bookmarx.log &
+echo $! > bookmarx.pid
